08bb74832599a941f406c3c3cf9b0087bcb3c133
BenTaotao <1573027560@qq.com>
2022-09-26 11:53:23 +0800

    更新验证唯一登陆的字符串

--- app/admin/middleware/Login.php
+++ app/admin/middleware/Login.php
@@ -19,11 +19,10 @@ class Login
      */
     public function handle($request, \Closure $next)
     {
-	
+
 		$Admin_outime = session('admin.outime');//登录过期时间  默认登录1小时过期
 		$Admin_lasttime = session('admin.lasttime'); //最近操作的时间
 		$admin_info = session('admin.info');
-	
 		$response = $next($request);
 		$controller = $request->controller();
 		$action = $request->action();
@@ -31,16 +30,16 @@ class Login
 		// halt($controller,$action,$app);
 		if (strtolower($controller) != "login") {
 			#这里验证session的存在性，过期时间，一个小时不操作等等的验证
+
 			if (!$admin_info || !$Admin_outime || ($Admin_outime < time()) || ((time() - $Admin_lasttime) > 3600)){
 				Session::delete('admin');
 				return redirect('/admin/login/index');
 			}
 
 			#这里验证是否是同一个人登录，异地登录,同一个账号不能有两个人同时登录
-			$login_str = Db::name('admin')->where(['id' => session('admin_info')['id']])->value('login_str');
-			if (session('login_str') != $login_str) {
-				Session::delete('Admin_outime');
-				Session::delete('admin_info');
+			$login_str = Db::name('admin')->where(['id' => $admin_info['id']])->value('login_str');
+			if (session('admin.login_str') != $login_str) {
+                Session::delete('admin');
 				return redirect('/admin/login/index');
 			}
 			#这里更新session的过期时间和最后操作时间
